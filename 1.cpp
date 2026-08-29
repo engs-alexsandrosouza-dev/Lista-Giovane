a097f3a2685750dd1e21a104058db224d7be29b3
Alex <engs-alexsandrosouza@camporeal.edu.br>
2026-08-29 18:59:56 -0300

    1

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+double AreaRetangulo(double base, double altura) {
+    return base * altura;
+}
+
+int main() {
+    double base, altura, area;
+    cout << "Digite a base do retângulo: ";
+    cin >> base;
+    cout << "Digite a altura do retângulo: ";
+    cin >> altura;
+
+
+    area = AreaRetangulo(base, altura);
+    cout << "A área do retângulo é: " << area << endl;
+
+    return 0;
+}
